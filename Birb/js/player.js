72c786f02035267deb7fbbd47800859a23103e49
RubenEsperto <75174154+RubenEsperto@users.noreply.github.com>
2024-12-03 11:25:19 +0000

    Drawing player

---
+++ Birb/js/player.js
@@ -0,0 +1,13 @@
+class Player{
+    constructor(game) {
+        this.game = game;
+        this.x = 50;
+        this.y = 60;
+        this.width = 100;
+        this.height = 100;
+    }
+
+    draw(){
+        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
