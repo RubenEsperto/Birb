1f7ae86e89a0caf530a732e3e9e3ec257fdf9dae
RubenEsperto <75174154+RubenEsperto@users.noreply.github.com>
2024-12-04 09:56:02 +0000

    Added window resizing

--- Birb/js/game.js
+++ Birb/js/game.js
@@ -5,10 +5,25 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
     }
 
+    resize(width, height) {
+            this.canvas.width = width;
+            this.canvas.height = height;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.ctx.fillStyle = '#17d1c8';
+    }
+
+    
+
     render() {
-        this.ctx.fillStyle = 'red';
         this.player.update();
         this.player.draw();
     }
